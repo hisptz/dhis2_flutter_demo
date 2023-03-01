30901ffa8027e8403a08fee453f6923be3b5f586
megamind <echingalo@gmail.com>
2023-03-01 19:51:08 +0300

    refactor: added the circular loader component

---
+++ lib/core/components/circular_loader.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CircularLoader extends StatelessWidget {
+  const CircularLoader({
+    Key? key,
+    this.color,
+    this.size,
+  }) : super(key: key);
+
+  final Color? color;
+  final double? size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: double.infinity,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(
+                strokeWidth: size ?? 4,
+                valueColor: AlwaysStoppedAnimation(
+                  color ?? Colors.white,
+                ),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
