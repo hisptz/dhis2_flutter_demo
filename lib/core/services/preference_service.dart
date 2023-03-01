6e7fdd6112cfe721aaafe6ee1c0729fff35c646c
megamind <echingalo@gmail.com>
2023-03-01 13:47:05 +0300

    feat: added the shared preference service

---
+++ lib/core/services/preference_service.dart
@@ -0,0 +1,15 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PreferenceService {
+  static setPreferenceValue(String preferenceKey, String value) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    await preferences.setString(preferenceKey, value);
+  }
+
+  static Future<String?> getPreferenceValue(
+    String preferenceKey,
+  ) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    return preferences.getString(preferenceKey);
+  }
+}
